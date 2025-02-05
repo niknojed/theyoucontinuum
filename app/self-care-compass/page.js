96f9a40a615db4c2d34aa50fd6647436873c3c44
Kinan Adams <design@alldazework.com>
2025-02-05 13:52:36 -0500

    updated broken links and small text

--- app/self-care-compass/page.js
+++ app/self-care-compass/page.js
@@ -221,8 +221,7 @@ The Self-Care Compass does all the scoring for you and provides immediate result
             </p>
 
             {/* CTA Button */}
-            <Link href="/self-care-compass">
-              <button className="font-ptSans flex items-center px-6 py-3 border-2 border-stone-500 bg-stone-500 text-white font-medium rounded-full shadow-md hover:border-stone-800 hover:bg-stone-800">
+            <button onClick={handleStart} className="font-ptSans flex items-center px-6 py-3 border-2 border-stone-500 bg-stone-500 text-white font-medium rounded-full shadow-md hover:border-stone-800 hover:bg-stone-800">
                   <span className="pr-2">
                   <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M22 12C22 17.5228 17.5228 22 12 22M22 12C22 6.47715 17.5228 2 12 2M22 12H20M12 22C6.47715 22 2 17.5228 2 12M12 22V20M2 12C2 6.47715 6.47715 2 12 2M2 12H4M12 2V4M19.0711 19.0711L17.6569 17.6569M6.34315 6.34315L4.92893 4.92893M17.6569 6.34315L19.0711 4.92893M4.92893 19.0711L6.34315 17.6569M8 12L10.5 10.5L12 8L13.5 10.5L16 12L13.5 13.5L12 16L10.5 13.5L8 12Z" 
@@ -232,7 +231,6 @@ The Self-Care Compass does all the scoring for you and provides immediate result
                   <span className="block md:hidden">Get Started</span> 
                   <span className="hidden md:block">Get started with the Self-Care Compass</span>
                 </button>
-            </Link>
           </div>
           
         </div>
